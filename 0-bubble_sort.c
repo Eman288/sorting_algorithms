69dbc750d9a2a9278f39aab0bd9fa464fecc46a1
Eman288 <emantamam107@gmail.com>
2023-09-24 01:51:24 -0700

    0x1B-task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function to sort the array with bubble sort algorithm
+ * @array: the array
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	int flag, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				flag = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		if (!flag)
+			break;
+	}
+}
