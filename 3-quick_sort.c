efa3540e0f0ac48e00d6d66f60a7192c482edfd5
Eman288 <emantamam107@gmail.com>
2023-09-25 06:26:47 -0700

    0x1B-task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,33 +9,32 @@
 */
 int parti(int *array, size_t size, int l, int h)
 {
-    int pivot, i, j, temp;
+	int pivot, i, j, temp;
 
-    pivot = array[l];
-    i = l;
-    j = h;
-    while (i < j)
-    {
-        do
-        {
-            i++;
-        } while (array[i] <= pivot);
-        do
-        {
-            j--;
-        } while (array[j] > pivot);
-        if (i < j)
-        {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            print_array(array, size);
-        }
-    }
-    temp = array[j];
-    array[j] = array[l];
-    array[l] = temp;
-    return j;
+	pivot = array[l];
+	i = l;
+	j = h;
+	while (i < j)
+	{
+		do {
+			i++;
+		} while (array[i] <= pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+		if (i < j)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
+	temp = array[j];
+	array[j] = array[l];
+	array[l] = temp;
+	print_array(array, size);
+	return (j);
 }
 /**
  * quicksort - the quick sort function
@@ -46,13 +45,14 @@ int parti(int *array, size_t size, int l, int h)
 */
 void quicksort(int *array, size_t size, int l, int h)
 {
-    int j;
-    if (h - l > 0)
-    {
-        j = parti(array, size, l, h);
-        quicksort(array, size, l, j);
-        quicksort(array, size, j + 1, h);
-    }
+	int j;
+
+	if (h - l > 0)
+	{
+		j = parti(array, size, l, h);
+		quicksort(array, size, l, j);
+		quicksort(array, size, j + 1, h);
+	}
 }
 /**
  * quick_sort - the main function
@@ -61,7 +61,7 @@ void quicksort(int *array, size_t size, int l, int h)
 */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-            quicksort(array, size, 0, size);
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, size, 0, size);
 }
