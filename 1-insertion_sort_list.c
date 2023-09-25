6f1da8262984783b29a59b69126527e393a23bf7
Eman288 <emantamam107@gmail.com>
2023-09-25 07:32:52 -0700

    0x1B-task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,27 +1,43 @@
 #include "sort.h"
 /**
+ * swab_nodes -  a function to swap two nodes
+ * @head: the header of the list
+ * @a: the first node
+ * @b: the second node
+ */
+void swap_nodes(listint_t **head, listint_t **a, listint_t *b)
+{
+	(*a)->next = b->next;
+	if (b->next != NULL)
+		b->next->prev = *a;
+	b->prev = (*a)->prev;
+	b->next = *a;
+	if ((*a)->prev != NULL)
+		(*a)->prev->next = b;
+	else
+		*head = b;
+	(*a)->prev = b;
+	*a = b->prev;
+}
+/**
  * insertion_sort_list - a function to sort the list with the inseration sort
- * list -  the list
+ *@list:  the list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *cur, *p, *g;
+	listint_t *g, *p, *cur, *temp;
 
 	temp = *list;
-	while (temp != NULL)
+	if (list == NULL || temp == NULL || temp->next == NULL)
+		return;
+	for (g = temp->next; g != NULL; g = cur)
 	{
-		cur = temp;
-		while (cur->prev != NULL && cur->n < cur->prev->n)
+		cur = g->next;
+		p = g->prev;
+		while (p != NULL && g->n < p->n)
 		{
-			g = cur;
-			p = g->prev;
-			p->prev->next = g;
-			g->prev = p->prev;
-			p->prev = g;
-			p->next = g->next;
-			g->next = p;
+			swap_nodes(list, &p, g);
 			print_list(*list);
 		}
-		temp = temp->next;
 	}
 }
